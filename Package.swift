9bdf2683365949d27d4cf35300a7a7586dcbdbda
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-01 06:31:25 +0100

    Update swift-kernel to 0.1.7, swift-mmap to 0.1.3

--- Package.swift
+++ Package.swift
@@ -30,9 +30,9 @@ let package = Package(
         .package(url: "https://github.com/swift-standards/swift-time-standard.git", from: "0.2.0"),
         .package(url: "https://github.com/swift-standards/swift-standards", from: "0.24.1"),
         .package(url: "https://github.com/apple/swift-nio", from: "2.70.0"),
-        .package(url: "https://github.com/coenttb/swift-kernel.git", from: "0.1.6"),
+        .package(url: "https://github.com/coenttb/swift-kernel.git", from: "0.1.7"),
         .package(url: "https://github.com/coenttb/swift-buffer.git", from: "0.1.0"),
-        .package(url: "https://github.com/coenttb/swift-mmap.git", from: "0.1.2"),
+        .package(url: "https://github.com/coenttb/swift-mmap.git", from: "0.1.3"),
     ],
     targets: [
         .target(
