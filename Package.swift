d85db96bff1c1efd774f5ebc5c8a0843e1c94df1
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-01 06:16:53 +0100

    Update swift-mmap to 0.1.2

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio", from: "2.70.0"),
         .package(url: "https://github.com/coenttb/swift-kernel.git", from: "0.1.6"),
         .package(url: "https://github.com/coenttb/swift-buffer.git", from: "0.1.0"),
-        .package(url: "https://github.com/coenttb/swift-mmap.git", from: "0.1.1"),
+        .package(url: "https://github.com/coenttb/swift-mmap.git", from: "0.1.2"),
     ],
     targets: [
         .target(
