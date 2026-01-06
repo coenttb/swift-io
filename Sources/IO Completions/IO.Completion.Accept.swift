aa1b4f3391cbd62898ed65c8a63d4c646b9f1771
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-06 22:07:40 +0100

    IO.Completion: Split Channel result types into individual files
    Split per Section 6.3:
    - Read.swift, Read.Result.swift
    - Write.swift, Write.Result.swift
    - Accept.swift, Accept.Result.swift
    - Connect.swift, Connect.Result.swift
    
    Channel.swift now contains only the Channel struct with methods
    that return the split result types.

---
+++ Sources/IO Completions/IO.Completion.Accept.swift
@@ -0,0 +1,11 @@
+//
+//  IO.Completion.Accept.swift
+//  swift-io
+//
+//  Created by Coen ten Thije Boonkkamp on 31/12/2025.
+//
+
+extension IO.Completion {
+    /// Namespace for accept-related types.
+    public enum Accept {}
+}
---
+++ Sources/IO Completions/IO.Completion.Write.swift
@@ -0,0 +1,11 @@
+//
+//  IO.Completion.Write.swift
+//  swift-io
+//
+//  Created by Coen ten Thije Boonkkamp on 31/12/2025.
+//
+
+extension IO.Completion {
+    /// Namespace for write-related types.
+    public enum Write {}
+}
